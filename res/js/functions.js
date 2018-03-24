e76d374e2b7e07c2306804ecd49374661bab18ca
Stoyan Anev <stoian4001@gmail.com>
2018-03-24 16:44:17 +0200

    new look

--- res/js/functions.js
+++ res/js/functions.js
@@ -1,19 +1,13 @@
 $(document).ready(function(){
-	$('.myGallery').hover(function(){
+		$(".socialIcons").hover(function(){
 
 			$(this).css({
-				'width': '300px',
-				'height': '300px',
-				'margin': '0px',
-				'box-shadow': '0px 0px 10px 1px black, 0px 0px 20px 15px white',
-				'transition': 'width 0.2s, height 0.2s, margin 0.2s, box-shadow 0.2s'})
+				"border-radius": "50%",
+				"transition": "border-radius 0.3s ease-in-out"})
 		}, function(){
 
 			$(this).css({
-				'width': '250px',
-				'height': '250px',
-				'margin': '30px',
-				'box-shadow': '0px 0px 6px black',
-				'transition': 'width 0.2s, height 0.2s, margin 0.2s, box-shadow 0.2s'})
+				"border-radius": "0%",
+				"transition": "border-radius 0.3s ease-in-out"})
 		});
 });
